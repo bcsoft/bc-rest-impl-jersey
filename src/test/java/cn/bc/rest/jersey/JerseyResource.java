f704bc1aa05a1cdeec061994c732aeac074c29ec
dragon <rongjihuang@gmail.com>
2016-07-28 18:28:35 +0800

    依赖于 jersey 的一些特殊方法测试

---
+++ src/test/java/cn/bc/rest/jersey/JerseyResource.java
@@ -0,0 +1,17 @@
+package cn.bc.rest.jersey;
+
+import javax.inject.Named;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * post 各种数据类型的请求测试
+ *
+ * @author dragon 2016-07-28
+ */
+@Named
+@Path("jersey")
+public class JerseyResource {
+
+}
